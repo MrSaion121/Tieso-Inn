73727985e0f1aefa96f235d63f4905d36602cd2a
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-10-06 10:29:29 -0600

    Archivo no necesitado

--- TiesoInn/src/controller/reservationController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-import { IReservation } from '../models/reservation';
-import { IUser } from '../models/user';
-
-
-//AutoIncrement "reservation_num"
-let reservationCounter = 1
