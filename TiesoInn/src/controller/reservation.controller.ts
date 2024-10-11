9fc877e35637f860fba31ca099ee444d51995c7c
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-10 21:38:14 -0600

    Implementacion de todo el CRUD de Reservations.

--- TiesoInn/src/controller/reservation.controller.ts
+++ TiesoInn/src/controller/reservation.controller.ts
@@ -23,116 +23,118 @@ class ReservationController {
             res.status(HTTP_STATUS_CODES.SUCCESS).json(reservations);
         } catch (error) {
             res.status(HTTP_STATUS_CODES.SERVER_ERROR).json({ message: 'Error al obtener las reservas', error });
+            console.log('Error:', error);
         }
     }
 
     //GET | ReservationById
     async getReservationById(req: Request, res: Response) {
-        const { id } = req.params;
+    const { id } = req.params;
 
-        try {
-            const reservation = await Reservation.findOne({ reservation_num: id }).populate('room_id', 'category_id');
-            if (!reservation) {
-                res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ message: 'Reserva no encontrada' });
-            }
-
-            res.status(HTTP_STATUS_CODES.SUCCESS).json(reservation);
-        } catch (error) {
-            res.status(HTTP_STATUS_CODES.SERVER_ERROR).json({ message: 'Error al obtener la reserva', error });
+    try {
+        const reservation = await Reservation.findOne({ reservation_num: id }).populate('room_id', 'category_id');
+        if (!reservation) {
+            res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ message: 'Reserva no encontrada' });
         }
+
+        res.status(HTTP_STATUS_CODES.SUCCESS).json(reservation);
+    } catch (error) {
+        res.status(HTTP_STATUS_CODES.SERVER_ERROR).json({ message: 'Error al obtener la reserva', error });
     }
+}
 
     // POST | createReservation
     async createReservation(req: Request, res: Response) {
-        const { email, room_id, arrival_date, checkout_date, num_of_guest, status } = req.body;
+    const { email, room_id, arrival_date, checkout_date, num_of_guest, status } = req.body;
 
-        try {
-            console.log("Datos recibidos en el cuerpo:", req.body);
+    try {
+        console.log("Datos recibidos en el cuerpo:", req.body);
 
-            // validacion de email del usuario
-            const userExists = await User.findOne({ email });
-            console.log("Usuario encontrado:", userExists);
+        const userExists = await User.findOne({ email });
+        const roomObjectId = mongoose.Types.ObjectId.isValid(room_id) ? new mongoose.Types.ObjectId(room_id) : null;
+        const roomExists = roomObjectId ? await Room.findById(roomObjectId) : null;
 
-            const roomObjectId = mongoose.Types.ObjectId.isValid(room_id) ? new mongoose.Types.ObjectId(room_id) : null;
-            console.log("ObjectId de la habitación generado:", roomObjectId);
-
-            const roomExists = roomObjectId ? await Room.findById(roomObjectId) : null;
-            console.log("Habitación encontrada:", roomExists);
-
-            // validacion si usuario o la habitación no existen = error
-            if (!userExists || !roomExists) {
-                console.log("Usuario o habitación no válidos");
-                return res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ message: 'Usuario o habitación no válidos' });
-            }
-
-            // Buscamos la última reservación para obtener el número más alto de reservación
-            const lastReservation = await Reservation.findOne().sort({ reservation_num: -1 });
-            const reservationNumber = lastReservation ? lastReservation.reservation_num + 1 : 1;
-            console.log("Nuevo número de reservación:", reservationNumber);
-
-            // Crea la nueva reservación
-            const newReservation = new Reservation({
-                reservation_num: reservationNumber, // Número de reserva auto-incremental
-                email,
-                room_id: roomObjectId,                      // ( _id de la habitacion)
-                arrival_date,
-                checkout_date,
-                num_of_guest,
-                status
-            });
-
-            console.log("Nueva reservación creada:", newReservation);
-
-            // almacenamos la reservación en la base de datos
-            await newReservation.save();
-            console.log("Reservación guardada correctamente");
-            res.status(HTTP_STATUS_CODES.CREATED).json(newReservation);
-
-        } catch (error) {
-            console.error('Error capturado al crear la reserva:', error);
-            res.status(HTTP_STATUS_CODES.SERVER_ERROR).json({ message: 'Error al crear la reserva' });
+        // validacion si usuario o la habitación no existen = error
+        if (!userExists || !roomExists) {
+            console.log("Usuario o habitación no válidos");
+            return res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ message: 'Usuario o habitación no válidos' });
         }
+
+        // Buscamos todos los números de reservación y los almacenamos en un array
+        const allReservationNumbers = await Reservation.find({}).select('reservation_num').lean();
+        const usedReservationNumbers = allReservationNumbers.map(reservation => reservation.reservation_num);
+
+        //Generacion del siguiente numero
+        let reservationNumber = 1;
+        while (usedReservationNumbers.includes(reservationNumber.toString())) {
+            reservationNumber++;
+        }
+
+        console.log("Nuevo número de reservación:", reservationNumber);
+
+        // Crea la nueva reservación
+        const newReservation = new Reservation({
+            reservation_num: reservationNumber.toString(), // Número de reserva auto-incremental
+            email,
+            room_id: roomObjectId,                      // ( _id de la habitacion)
+            arrival_date,
+            checkout_date,
+            num_of_guest,
+            status
+        });
+
+        console.log("Nueva reservación creada:", newReservation);
+
+        // almacenamos la reservación en la base de datos
+        await newReservation.save();
+        console.log("Reservación guardada correctamente");
+        res.status(HTTP_STATUS_CODES.CREATED).json(newReservation);
+
+    } catch (error) {
+        console.error('Error capturado al crear la reserva:', error);
+        res.status(HTTP_STATUS_CODES.SERVER_ERROR).json({ message: 'Error al crear la reserva' });
     }
+}
 
     //POST | UpdateReservation
     async updateReservation(req: Request, res: Response) {
-        const { id } = req.params;
-        const { email, room_id, arrival_date, checkout_date, num_of_guest, status } = req.body;
+    const { id } = req.params;
+    const { email, room_id, arrival_date, checkout_date, num_of_guest, status } = req.body;
 
-        try {
-            const updatedReservation = await Reservation.findOneAndUpdate(
-                { reservation_num: id },
-                { email, room_id, arrival_date, checkout_date, num_of_guest, status },
-                { new: true }
-            );
+    try {
+        const updatedReservation = await Reservation.findOneAndUpdate(
+            { reservation_num: id },
+            { email, room_id, arrival_date, checkout_date, num_of_guest, status },
+            { new: true }
+        );
 
-            if (!updatedReservation) {
-                return res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ message: 'Reserva no encontrada' });
-            }
-
-            res.status(HTTP_STATUS_CODES.SUCCESS).json(updatedReservation);
-
-        } catch (error) {
-            res.status(HTTP_STATUS_CODES.SERVER_ERROR).json({ message: 'Error al actualizar la reservacion', error });
+        if (!updatedReservation) {
+            return res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ message: 'Reserva no encontrada' });
         }
+
+        res.status(HTTP_STATUS_CODES.SUCCESS).json(updatedReservation);
+
+    } catch (error) {
+        res.status(HTTP_STATUS_CODES.SERVER_ERROR).json({ message: 'Error al actualizar la reservacion', error });
     }
+}
 
     //DELETE
     async deleteReservation(req: Request, res: Response) {
-        const { id } = req.params;
+    const { id } = req.params;
 
-        try {
-            const deletedReservation = await Reservation.findOneAndDelete({ reservation_num: id });
+    try {
+        const deletedReservation = await Reservation.findOneAndDelete({ reservation_num: id });
 
-            if (!deletedReservation) {
-                return res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ message: 'Reserva no encontrada' });
-            }
-
-            res.status(HTTP_STATUS_CODES.SUCCESS).json({ message: 'Reserva Eliminada Correctamente' });
-        } catch (error) {
-            res.status(HTTP_STATUS_CODES.SERVER_ERROR).json({ message: 'Error al eliminar la reserva', error });
+        if (!deletedReservation) {
+            return res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ message: 'Reserva no encontrada' });
         }
+
+        res.status(HTTP_STATUS_CODES.SUCCESS).json({ message: 'Reserva Eliminada Correctamente' });
+    } catch (error) {
+        res.status(HTTP_STATUS_CODES.SERVER_ERROR).json({ message: 'Error al eliminar la reserva', error });
     }
+}
 }
 
 export const reservationController = new ReservationController();
--- TiesoInn/src/models/reservation.ts
+++ TiesoInn/src/models/reservation.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema, Document } from 'mongoose';
 //Model/Interfaz para reservaciones
 export interface IReservation extends Document {
     reservation_num: string;
-    email: string;
+    email: mongoose.Schema.Types.ObjectId;
     room_id: mongoose.Schema.Types.ObjectId;
     arrival_date: Date;
     checkout_date: Date;
@@ -15,11 +15,11 @@ export interface IReservation extends Document {
 const reservationSchema: Schema = new Schema({
     reservation_num: { type: String, required: true, unique: true },
     email: { type: String, ref: 'User', required: true },
-    room_id: { type: Schema.Types.ObjectId, ref: 'Room', required: true },
+    room_id: { type: Schema.Types.ObjectId, ref: 'room', required: true },
     arrival_date: { type: Date, required: true },
     checkout_date: { type: Date, required: true },
     num_of_guest: { type: Number, required: true },
-    status: { type: String, default: 'pending' }
+    status: { type: String, enum:  ['Pagado', 'Pendiente', 'Cancelado', 'Confirmado'], default: 'Pendiente' }
 });
 
 export default mongoose.model<IReservation>('Reservation', reservationSchema);
