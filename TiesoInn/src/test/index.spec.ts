117465a500d543dae2e4c509fd259dc067664f44
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-11-23 15:22:43 -0600

    Creacion de config de JEST para pruebas unitarias, Se creo la prueba unitaria princial, que es validar que arranque el servidor.

--- TiesoInn/src/test/index.spec.ts
+++ TiesoInn/src/test/index.spec.ts
@@ -1,9 +1,30 @@
 import request from 'supertest';
-import app from '../index';
+//import app from '../index';
+import { HTTP_STATUS_CODES } from '../types/http-status-codes';
+//Cargar variables de entorno
+import dotenv from 'dotenv';
+dotenv.config();
+const SERVER_URL = process.env.SERVER_URL || '';
+const PORT = process.env.PORT || 3000;
+//url del servidor
+const serverUrl = `${SERVER_URL}:${PORT}`
 
-describe('Base API Tests', () => {
-    it('should return a 404 for undefined routes', async () => {
-        const response = await request(app).get('/non-existent-route');
-        expect(response.status).toBe(404);
+//Creacion prueba
+describe('Arranque de Servidor basico', () => {
+
+    //Caso 1: verificar rutas desconocidas
+    it('Debe iniciar y responder con un estatus 404 para rutas desconocidas', async () => {
+        const response = await request(serverUrl).get('/ruta-desconocida'); //Realiza la solicitud a una ruta random
+
+        //Expectativa de resultado
+        expect(response.statusCode).toBe(HTTP_STATUS_CODES.NOT_FOUND); //Validar el estatus
+        //expect(response.body).toEqual({}); //Si no hay body CODE : 404 | NOT Found
     });
+
+    //Caso 2: Verificar respuesta HTML
+    it('Debe Responder con el HTML de login', async () => {
+        const response = await request(serverUrl).get('/login'); //Realiza la solicitud a ruta main
+        expect(response.statusCode).toBe(HTTP_STATUS_CODES.SUCCESS); // Verificar que responda exitosamente
+        expect(response.text).toContain('<html'); //Asegurarse que contenga HTML
+    })
 });
