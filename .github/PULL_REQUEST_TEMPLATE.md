668d6280e8e2d1701c6cc323e289368534f574ef
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2025-03-05 12:07:19 -0600

    chore: adding more GHA

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,15 @@
+### Description
+- What does this PR do?
+- Why is this change necessary?
+
+### Related issue
+- Closes #<issue number> or references related issue(s)
+
+### How to test
+- What steps can be taken to test this PR?
+
+### Considerations:
+- [ ] I have read the contributing guidelines.
+- [ ] My code follows the code style of this project.
+- [ ] I have tested my changes locally.
+- [ ] I have updated documentation where necessary.
---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        languageOptions: { globals: globals.browser }
+    },
+    {
+        ignores: ['dist/', 'node_modules/']
+    },
+    {
+        files: ['src/**/*.ts'],
+        rules: {
+            '@typescript-eslint/explicit-module-boundary-types': 'warn',
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/ban-ts-comment': 'warn',
+            'no-console': ['warn', { allow: ['warn', 'error'] }],
+            'eqeqeq': ['error', 'always'],
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single'],
+            'indent': ['error', 2],
+            'curly': ['error', 'all'],
+            'consistent-return': 'warn',
+            'brace-style': ['error', '1tbs'],
+            'arrow-parens': ['error', 'always'],
+            'no-trailing-spaces': 'error',
+            'space-before-function-paren': ['error', 'never'],
+            'no-throw-literal': 'error',
+            'prefer-const': 'error',
+            'no-debugger': 'error',
+            'no-empty-function': 'warn',
+            'no-empty': 'warn',
+        },
+    },
+];
