9bd7dd19e2fbd2c00c46eb499f99374d540d9610
Santiago Oseguera <142419366+MrSaion121@users.noreply.github.com>
2025-03-22 14:04:54 -0600

    fix(lint): resolve ESLint errors in project
    Fixed multiple ESLint issues including unused variables and formatting inconsistencies.

--- src/types/passport.d.ts
+++ src/types/passport.d.ts
@@ -1,7 +1,7 @@
 import { AuthUserPayload } from '../middlewares/auth';
 
-// eslint-disable-next-line  @typescript-eslint/no-empty-object-type
 declare module 'express-serve-static-core' {
+    // eslint-disable-next-line  @typescript-eslint/no-empty-object-type
     interface User extends AuthUserPayload {}
 }
 
