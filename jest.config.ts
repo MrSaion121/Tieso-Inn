cfde9062275a07a97c1247cfa83faf58accee115
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2025-05-12 17:30:57 -0600

    chore: removing comments from jest threshold

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,14 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 80,
-  //     functions: 80,
-  //     lines: 80,
-  //     statements: 80,
-  //   },
-  // },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
 
 export default config;
